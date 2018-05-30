0bdd69c64a7c39629112f453778ba3e47e9db5f8
GomesRui <39780267+GomesRui@users.noreply.github.com>
2018-05-30 23:20:00 +0100

    Add files via upload

---
+++ DABOT SRC/srcDaBotHeader.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string> 
+#include <exception>
+
+//using namespace std;
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::vector;
+using std::min;
+using std::max;
+using std::to_string;
+using std::endl;
+using std::exception;
